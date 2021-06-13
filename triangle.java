1f98a5f16cd7be9bb3aac03e2b282af15c017b10
chirag-code18 <chiragadvani1812@gmail.com>
2021-06-13 21:06:07 +0530

    adding a file

---
+++ triangle.java
@@ -0,0 +1,28 @@
+package pyamid;
+
+import java.util.*;
+
+public class triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n=sc.nextInt();
+		while(i<n)
+		{
+			int j=0;
+			while(j<(i+1))
+			{
+				System.out.print("*");
+				System.out.print(" ");
+				j++;
+			}
+		System.out.println(" ");
+		i++;
+		}
+
+	}
+
+}
